d64f08fd2974ec88dcd6563873b3e108b0c3205d
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-02-10 23:28:32 -0500

    added the start of a validation page, which will later be moved to a class

---
+++ model/validation_functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/*this is a placeholder validator*/
+function validUserName($user)
+{
+    return strlen($user) >= 3;
+}
+
+/*this is a placeholder validator*/
+function validPassword($pass)
+{
+    return strlen($pass) >= 3;
+}
