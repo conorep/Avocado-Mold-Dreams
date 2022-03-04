dcc2a9ca37f31c2b687d5220257c382fe716d2aa
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-03-04 00:52:46 -0500

    made a normal user class. needs a ton of work, only has constructor and some fields so far. will likely extend to admin...?

---
+++ classes/amd-user.php
@@ -0,0 +1,14 @@
+<?php
+
+    //TODO: a lot of this
+    class AMDUser
+    {
+        private $_fname;
+        private $_lname;
+        private $_phoneNum;
+        private $_email;
+        private $_userID;
+
+        public function __construct()
+        {}
+    }
