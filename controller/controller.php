e4a0183ebeb2de0392f5998875c191dc24cc5225
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-03-09 00:49:56 -0500

    added some pics to the inventory folder, added rows to the DB that pull them up. not much validation of the forms for this stuff or the actual image upload yet, but we be getting there. controller is way too big, way too long, need to split things up into more methods, but one step at a time. admin view has some minor errors sections, so there is a tiny bit of validating.

--- controller/controller.php
+++ controller/controller.php
@@ -56,9 +56,9 @@
 
                     // Check if image file is actual or fake image
                     if(isset($_POST["submit"])) {
-                        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                        $check = getimagesize($_FILES["uploadPic"]["tmp_name"]);
                         if($check !== false) {
-                            echo "File is an image - " . $check["mime"] . ".";
+                            /*echo "File is an image - " . $check["mime"] . ".";*/
                             $uploadOk = 1;
                         } else {
                             echo "File is not an image.";
@@ -66,12 +66,14 @@
                         }
                     }
                     // Check if file already exists
+                    //TODO: note that this error check doesn't work, because I wills till be sending a file name
+                    // for an existing file to the DB. gotta be careful. validations to come!
                     if (file_exists($target_file)) {
                         echo "Sorry, file already exists.";
                         $uploadOk = 0;
                     }
                     // Check file size
-                    if ($_FILES["fileToUpload"]["size"] > 500000) {
+                    if ($_FILES["uploadPic"]["size"] > 500000) {
                         echo "Sorry, your file is too large.";
                         $uploadOk = 0;
                     }
@@ -85,7 +87,7 @@
                         echo "Sorry, your file was not uploaded.";
                     // if everything is ok, try to upload file
                     } else {
-                        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                        if (move_uploaded_file($_FILES["uploadPic"]["tmp_name"], $target_file)) {
                             $this->_f3->set('picUp["newPicUp"]', 'Picture uploaded successfully.');
                         } else {
                             $this->_f3->set('picUp["noPicUp"]', 'Error. Picture did not upload.');
@@ -112,6 +114,7 @@
 
                     if(empty($this->_f3->get('errors'))) {
                         $GLOBALS['dataLayer']->addNewItem($itemName, $itemPrice, $itemDesc, $sendName);
+                        $itemName = ""; $itemPrice = ""; $itemQty = "";
                     }
 
                 }
