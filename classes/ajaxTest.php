296fcdfb68a0b096f4870bbee8ea5ab4d5df3ad1
Pat Tagliavia <pat.tagliavia@gmail.com>
2022-03-10 00:15:00 -0500

    added relative path to home.html jquery/ajax, and updated ajaxTest code

--- classes/ajaxTest.php
+++ classes/ajaxTest.php
@@ -1,5 +1,5 @@
 <?php
 //testing alteration to the session here from the 'add to cart' click on the main page
 session_start();
-$_SESSION['sessionState'] = "we changed the session 2";
+$_SESSION['sessionState'] = "we changed the session 4";
 print_r("this page changes session variables, in theory (confirmed reachable by url I'm using with ajax with this print from absolute url)");
