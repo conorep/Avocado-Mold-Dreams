906da37838212edcc836fee2e16328ff28fd605f
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-03-12 03:33:45 -0500

    removed some edited stuff i'd done previously and tried to get rid of vardump on fist page. dont know where that's coming from, not index i guess

--- classes/ajaxTest.php
+++ classes/ajaxTest.php
@@ -5,30 +5,10 @@ ob_start();
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-require_once('Cart.php');
+require_once('cart.php');
 
 //testing alteration to the session here from the 'add to cart' click on the main page
 session_start();
-/*echo("SESSION: <BR>");
-var_dump($_SESSION);
-
-echo("<br>POST: <BR>");
-var_dump($_POST);
-$_SESSION['sessionState'] = "we changed the session 5";
-print_r("this page changes session variables, in theory (confirmed reachable by url I'm using with ajax with this print from absolute url)");
-//$_SESSION['sessionResult'] = (int)$_POST['val'];*/
-
 $userCart= new Cart();
 $userCart->addToCart((int)$_POST['val']);
 $_SESSION['sessionCart'] = $userCart;
-
-
-/*    if(isset($_SESSION['itemIDsForCart'])){
-        unset($thisArray);
-        $thisArray[] = $_SESSION['itemIDsForCart'];
-        $thisArray[] = $_POST['myVar'];
-        $_SESSION['itemIDsForCart'] = $thisArray;
-    } else {
-        $thisArray[] = $_POST['val'];
-        $_SESSION['itemIDsForCart'] = $thisArray;
-    }*/
